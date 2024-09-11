8745ba812d20c26a08d41d235436c48dd759f081
sangjun <byjun0302@gmail.com>
2024-09-11 18:43:37 +0900

    feat: add tabBarControl to rootTab

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -9,15 +9,62 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin{
+  late TabController controller;
+
+
   int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    controller = TabController(length: 4, vsync: this);
+
+    controller.addListener(tabListener);
+  }
+  
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
+
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '''Jun's Delivery''',
-      child: Center(
-        child: Text('Root Tab'),
+      child: TabBarView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: [
+          Center(
+            child: Container(
+              child: Text('홈'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('음식'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('주문'),
+            ),
+          ),
+          Center(
+            child: Container(
+              child: Text('프로필'),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: PRIMARY_COLOR,
@@ -26,24 +73,25 @@ class _RootTabState extends State<RootTab> {
         unselectedFontSize: 10,
         type: BottomNavigationBarType.fixed,
         onTap: (int index) {
-          setState(() {
-            this.index = index;
-          });
+          controller.animateTo(index);
         },
         currentIndex: index,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: '홈',
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.fastfood_outlined),
-            label: '홈',
+            label: '음식',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.receipt_outlined),
-            label: '홈',
+            icon: Icon(Icons.receipt_long_outlined),
+            label: '주문',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: '홈',
+            icon: Icon(Icons.person_outlined),
+            label: '프로필',
           )
         ],
       ),
