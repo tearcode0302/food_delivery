44ee051af8284b475c15e72e351d0f8a2a02c0d8
sangjun <byjun0302@gmail.com>
2024-10-09 11:28:49 +0900

    feat: add CustomScrollView to restaurant_detail_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery/user/view/login_screen.dart';
 import 'package:food_delivery/user/view/splash_screen.dart';
 
 void main() {
@@ -9,7 +8,6 @@ void main() {
 }
 
 class _App extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,5 +19,3 @@ class _App extends StatelessWidget {
     );
   }
 }
-
-
--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -11,6 +11,49 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderProducts(),
+        ],
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
       child: Column(
         children: [
           RestaurantCard(
@@ -28,9 +71,10 @@ class RestaurantDetailScreen extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
-              child: ProductCard()),
+            child: ProductCard(),
+          ),
         ],
-      )
+      ),
     );
   }
 }
